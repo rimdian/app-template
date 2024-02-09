da1586e1ff651526b3e834853b69b052003608b8
Pierre Bazoge <pierre@bazoge.com>
2024-02-09 22:09:57 +0100

    webhook signature verification

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -2,10 +2,20 @@ import { WebhookResult, WebhookPayload } from '@/interfaces'
 import processDataHooks from '@/processDataHooks'
 import processTasks from '@/processTasks'
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { buffer } from 'micro'
 
 export const SignatureHeader = 'X-Rmd-Signature'
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<WebhookResult>) {
+export const config = {
+  // Specifies the maximum allowed duration for this function to execute (in seconds)
+  maxDuration: 20,
+  api: {
+    // Disables body parsing to compute the signature on the raw body properly
+    bodyParser: false
+  }
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<WebhookResult>) {
   if (req.method !== 'POST') {
     // The request is not a POST request
     res.status(200).end('This endpoint only accepts POST requests')
@@ -13,7 +23,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Webhoo
   }
 
   // validate Signature header
-  const signature = req.headers[SignatureHeader]
+  const signature = req.headers[SignatureHeader.toLowerCase()]
   if (!signature) {
     console.error('Missing Signature header ' + SignatureHeader)
     res.status(401).end('Missing Signature header')
@@ -27,15 +37,13 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Webhoo
     return
   }
 
-  const payload = req.body as WebhookPayload
-  console.log('webhook body', payload)
+  const rawBody = (await buffer(req)).toString()
+
+  // console.log('Received payload', rawBody)
 
   // compute signature
   const crypto = require('crypto')
-  const computedSignature = crypto
-    .createHmac('sha256', appSecretKey)
-    .update(JSON.stringify(payload))
-    .digest('hex')
+  const computedSignature = crypto.createHmac('sha256', appSecretKey).update(rawBody).digest('hex')
 
   // compare signatures
   if (signature !== computedSignature) {
@@ -44,38 +52,42 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Webhoo
     return
   }
 
+  // parse payload
+  let payload: WebhookPayload
+  try {
+    payload = JSON.parse(rawBody)
+  } catch (error) {
+    console.error('Error parsing payload', error)
+    res.status(400).end('Error parsing payload ' + error)
+    return
+  }
+
   // process payload
   if (payload.kind === 'data_hook') {
-    processDataHooks(payload)
-      .then((result: WebhookResult) => {
-        res.status(200).json(result)
-        return
-      })
-      .catch((error: any) => {
-        console.error('Error processing data hook', error)
-        res.status(500).end('Error processing data hook ' + error)
-        return
-      })
+    try {
+      const result = await processDataHooks(payload)
+      res.status(200).json(result)
+      return
+    } catch (error) {
+      console.error('Error processing data hook', error)
+      res.status(500).end('Error processing data hook ' + error)
+      return
+    }
   }
 
   if (payload.kind === 'task_exec_worker') {
-    processTasks(payload)
-      .then((result: WebhookResult) => {
-        res.status(200).json(result)
-      })
-      .catch((error: any) => {
-        console.error('Error processing task', error)
-        res.status(500).end('Error processing task ' + error)
-      })
-    return
+    try {
+      const result = await processTasks(payload)
+      res.status(200).json(result)
+      return
+    } catch (error) {
+      console.error('Error processing task', error)
+      res.status(500).end('Error processing task ' + error)
+      return
+    }
   }
 
   // unknown kind
   console.error('Unknown webhook kind', payload.kind)
   res.status(400).end('Unknown webhook kind: ' + payload.kind)
-}
-
-export const config = {
-  // Specifies the maximum allowed duration for this function to execute (in seconds)
-  maxDuration: 20
 }
